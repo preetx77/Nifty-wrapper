e350c88a5d924fbed78e39a0f122020cbbdea3f2
Preet <preetsonar77@gmail.com>
2026-06-01 21:28:12 +0530

    Fix: resolve syntax errors in main.py - remove duplicate code blocks

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from fetch_data import get_nifty_data
+from metrics import (
+    fifty_two_week_high, 
+    fifty_two_week_low , 
+    annualized_volatility,
+    moving_averages
+)
+
+nifty = get_nifty_data()
+high = fifty_two_week_high(nifty)
+low = fifty_two_week_low(nifty)
+volatility = annualized_volatility(nifty)
+
+dma20 = moving_averages(nifty, 20)
+dma50 = moving_averages(nifty, 50)
+dma200 = moving_averages(nifty, 200)
+
+
+print(f"25 Week High : {high:.2f}")
+print(f"25 Week Low : {low:.2f}")
+print(f"Anuallized Volatility :  {volatility:.2f}%")
+
+print(f"20 DMA  : {dma20:.2f}")
+print(f"50 DMA  : {dma50:.2f}")
+print(f"200 DMA : {dma200:.2f}")
