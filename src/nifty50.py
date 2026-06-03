f072e3c28b507e8d229008fe01c41d3beddc934e
Preet <preetsonar77@gmail.com>
2026-06-03 21:56:35 +0530

    Add: nifty50.py - list of NIFTY 50 stocks

---
+++ src/nifty50.py
@@ -0,0 +1,14 @@
+NIFTY50 = [
+
+    "RELIANCE.NS",
+    "TCS.NS",
+    "HDFCBANK.NS",
+    "INFY.NS",
+    "ICICIBANK.NS",
+    "SBIN.NS",
+    "LT.NS",
+    "ITC.NS",
+    "BHARTIARTL.NS",
+    "AXISBANK.NS",
+
+]
