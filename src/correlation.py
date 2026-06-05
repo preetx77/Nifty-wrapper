1ed73974f5efe6e3ccb566785432aaa89c810ca2
Preet <preetsonar77@gmail.com>
2026-06-05 22:14:44 +0530

    Update: correlation.py - add market correlation analysis

--- src/correlation.py
+++ src/correlation.py
@@ -13,13 +13,13 @@ def daily_returns(df):
 
 def correlation(series1, series2):
 
-    merged = pd.concat(
-        [series1, series2],
-        axis=1
-    ).dropna()
-
-    corr = merged.iloc[:, 0].corr(
-        merged.iloc[:, 1]
-    )
+    merged = pd.concat([series1, series2],axis=1).dropna()
+    corr = merged.iloc[:, 0].corr(merged.iloc[:, 1])
 
     return corr
+
+def rolling_correlation(series1, series2, window=30):
+    merged = (pd.concat([series1, series2], axis=1).dropna())
+    rolling_corr = (merged.iloc[:, 0].rolling(window).corr(merged.iloc[:,1] ))
+    
+    return rolling_corr
