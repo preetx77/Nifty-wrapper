7f79f63965e375ea5abb9caebc4d70b7d7945354
Preet <preetsonar77@gmail.com>
2026-06-10 22:03:19 +0530

    graphs plot for correaltion

--- src/visualize.py
+++ src/visualize.py
@@ -14,3 +14,29 @@ def plot_nifty_trend(df):
     plt.legend()
     plt.grid(True)
     plt.show()
+
+
+def plot_vix(vix):
+    plt.figure(figsize= (12,5))
+    plt.plot(vix.index,vix["Close"], label = "India Vix")
+
+    plt.title("INDIA VIX")
+    plt.xlabel("Date")
+    plt.ylabel("VIX")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+# rolling correlation chart
+def plot_rolling_correaltion(rolling_corr):
+    plt.figure(figsize=(12, 5))
+    plt.plot(rolling_corr.index, rolling_corr, label="Rolling Correlation")
+    plt.axhline(y=0, linestyle="--")
+    plt.title("NIFTY vs VIX correaltion")
+    plt.xlabel("Date")
+    plt.ylabel("Correaltion")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+    
